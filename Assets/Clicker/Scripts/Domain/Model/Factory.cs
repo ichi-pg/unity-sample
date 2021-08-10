c2060839b94adaadc5f112ed65e880f02fbe623a
ichi-pg <ichi.colors@gmail.com>
2021-08-10 18:48:17 +0900

    clicker

---
+++ Assets/Clicker/Scripts/Domain/Model/Factory.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Clicker
+{
+    public class Factory
+    {
+        public int Level { get; private set; }
+    }
+}
---
+++ Assets/Clicker/Scripts/Domain/Model/Player.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Clicker
+{
+    public class Player
+    {
+        public int Coin { get;private set; }
+    }
+}
