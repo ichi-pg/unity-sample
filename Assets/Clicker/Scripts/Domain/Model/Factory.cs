7bccd55e0c4dcbdd08f36ef3ad17bb1f6864ec60
ichi-pg <ichi.colors@gmail.com>
2021-08-11 18:43:00 +0900

    ignore and comment

--- Assets/Clicker/Scripts/Domain/Model/Factory.cs
+++ Assets/Clicker/Scripts/Domain/Model/Factory.cs
@@ -8,6 +8,7 @@ namespace Clicker
         public int Level { get; private set; }
 
         //TODO 単純に Factory = 女の子 でいいんじゃない（カフェ、農園、メイド、基地、冒険者）？
+        //TODO 正攻法だと精霊、道具、商品、土地、施設
         //TODO Factory = Merge のパターン
         //TODO 放置で増える
     }
