cb3425dd9893053f6b04366d9ca5a26726e4da05
ichi-pg <ichi.colors@gmail.com>
2021-08-18 00:21:18 +0900

    opt calc

--- Assets/Clicker/Scripts/Domain/Model/FactoryCalculator.cs
+++ Assets/Clicker/Scripts/Domain/Model/FactoryCalculator.cs
@@ -10,10 +10,10 @@ namespace Clicker
         public float Interval { get => 1.0f; }
 
         public BigInteger Power(int level, int rank) {
-            return new BigInteger(level) * rank * rank;
+            return new BigInteger(level) * rank * (rank + 4);
         }
         public BigInteger Cost(int level, int rank) {
-            return new BigInteger(level) * level * rank + 20 * rank * rank;
+            return new BigInteger(level) * level * rank + 20 * rank * rank * rank;
         }
     }
 }
