0f3fd5f05174e2eb0bf75de65b6ccd072ebe9359
ichi-pg <ichi.colors@gmail.com>
2021-08-23 16:36:05 +0900

    time produce limit

--- Assets/Ichi.Clicker/Scripts/Domain/Model/Produce/TimeProducer.cs
+++ Assets/Ichi.Clicker/Scripts/Domain/Model/Produce/TimeProducer.cs
@@ -6,15 +6,18 @@ namespace Ichi.Clicker
 {
     public class TimeProducer : IProducer
     {
-        private long interval = 1000;
+        private long interval = 1000;//TODO ブースト
+        private long limit = 12*60*60*1000;//TODO 広告で2倍
 
         public bool Produce(IStore store, BigInteger power, long now, ref long producedAt) {
             if (now < producedAt) {
                 return false;
             }
-            //TODO リミッター
-            //TODO ブースト
-            var count = (now - producedAt) / this.interval;
+            var span = now - producedAt;
+            if (span > limit) {
+                span = limit;
+            }
+            var count = span / this.interval;
             if (!store.Store(power * count)) {
                 return false;
             }
