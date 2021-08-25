0f22ee2db8dfc8809e720400588e92448d2d68a5
ichi-pg <ichi.colors@gmail.com>
2021-08-25 21:09:15 +0900

    opt code

--- Assets/Ichi.Clicker/Scripts/Domain/Repository/Offline/SaveData.cs
+++ Assets/Ichi.Clicker/Scripts/Domain/Repository/Offline/SaveData.cs
@@ -37,11 +37,15 @@ namespace Ichi.Clicker
         public List<Factory> factories;
         public List<Item> items;
         public long nextFeverTicks;
-        public DateTime NextFeverAt { get => new DateTime(this.nextFeverTicks); set => this.nextFeverTicks = value.Ticks; }
         public Item Coin { get; private set; }
         public Item Product { get; private set; }
         public IEnumerable<Factory> ClickFactories { get; private set; }
         public IEnumerable<Factory> AutoFactories { get; private set; }
+
+        public DateTime NextFeverAt {
+            get => new DateTime(this.nextFeverTicks);
+            set => this.nextFeverTicks = value.Ticks;
+        }
 
         private SaveData() {
         }
