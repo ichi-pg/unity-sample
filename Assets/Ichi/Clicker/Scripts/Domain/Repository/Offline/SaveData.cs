481183ebf7ccdd0c261aa9ca5c1b1b7066a0bb4f
ichi-pg <ichi.colors@gmail.com>
2021-08-31 11:39:38 +0900

    opt code

--- Assets/Ichi/Clicker/Scripts/Domain/Repository/Offline/SaveData.cs
+++ Assets/Ichi/Clicker/Scripts/Domain/Repository/Offline/SaveData.cs
@@ -12,27 +12,13 @@ namespace Ichi.Clicker.Offline
         public static SaveData Instance {
             get {
                 if (instance == null) {
-                    var now = Common.Time.Now;
                     if (Common.JsonSaveData.Exist<SaveData>()) {
                         instance = Common.JsonSaveData.Load<SaveData>();
                         //NOTE クラウドセーブ
                     } else {
-                        instance = new SaveData() {
-                            factories = new List<Factory>(),
-                            items = new List<Item>(),
-                            NextFeverAt = now,
-                            NextFeverAdsAt = now,
-                        };
+                        instance = new SaveData(Common.Time.Now);
                     }
-                    Initializer.Initialize(
-                        instance.factories,
-                        instance.items
-                    );
-                    instance.ClickFactories = instance.factories.Where(factory => factory.category == (int)FactoryCategory.Click);
-                    instance.AutoFactories = instance.factories.Where(factory => factory.category == (int)FactoryCategory.Auto);
-                    instance.Coin = instance.items.FirstOrDefault(item => item.category == (int)ItemCategory.Coin);
-                    instance.Product = instance.items.FirstOrDefault(item => item.category == (int)ItemCategory.Product);
-                    instance.LoginProduct = instance.items.FirstOrDefault(item => item.category == (int)ItemCategory.LoginProduct);
+                    instance.Initialize();
                 }
                 return instance;
             }
@@ -58,7 +44,11 @@ namespace Ichi.Clicker.Offline
             set => this.nextFeverAdsTicks = value.Ticks;
         }
 
-        private SaveData() {
+        private SaveData(DateTime now) {
+            this.factories = new List<Factory>();
+            this.items = new List<Item>();
+            this.NextFeverAt = now;
+            this.NextFeverAdsAt = now;
         }
 
         public void Save() {
@@ -75,6 +65,18 @@ namespace Ichi.Clicker.Offline
             foreach (var item in this.items) {
                 item.quantity.PostLoad();
             }
+        }
+
+        public void Initialize() {
+            Initializer.Initialize(
+                this.factories,
+                this.items
+            );
+            this.ClickFactories = this.factories.Where(factory => factory.category == (int)FactoryCategory.Click);
+            this.AutoFactories = this.factories.Where(factory => factory.category == (int)FactoryCategory.Auto);
+            this.Coin = this.items.FirstOrDefault(item => item.category == (int)ItemCategory.Coin);
+            this.Product = this.items.FirstOrDefault(item => item.category == (int)ItemCategory.Product);
+            this.LoginProduct = this.items.FirstOrDefault(item => item.category == (int)ItemCategory.LoginProduct);
         }
     }
 }
