817ab5430a477fff5f4acec8992de21cfcdf238d
ichi-pg <ichi.colors@gmail.com>
2021-09-06 18:14:41 +0900

    comment

--- Assets/Ichi/Rouge/Scripts/Domain/Model/Battle.cs
+++ Assets/Ichi/Rouge/Scripts/Domain/Model/Battle.cs
@@ -6,7 +6,8 @@ namespace Ichi.Rouge
     public class Battle
     {
         //NOTE Battle = 強制Win で一旦ローグ部分だけ作ればいいじゃない
-        //NOTE Battle = TourDeffence のパターン
-        //NOTE Battle = Ichi.Clicker のパターン
+        //NOTE ＋タワーディフェンス
+        //NOTE ＋クリッカー
+        //NOTE ＋2D、3Dアクション
     }
 }
