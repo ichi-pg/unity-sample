7dfc20e004d48fa8e31e954c0e7a88adbd75185e
ichi-pg <ichi.colors@gmail.com>
2021-09-15 13:48:14 +0900

    to unirx

--- Assets/Ichi/Common/Scripts/View/Novel/NovelView.cs
+++ Assets/Ichi/Common/Scripts/View/Novel/NovelView.cs
@@ -1,24 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using System;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
 
 namespace Ichi.Common
 {
     public class NovelView : MonoBehaviour
     {
         [SerializeField]
-        private WaitTapButton waitTapButton;
+        private Button button;
         [SerializeField]
         private Text text;
 
         public async UniTask Play(IEnumerable<INovel> novels) {
-            var token = this.GetCancellationTokenOnDestroy();
             foreach (var novel in novels) {
                 this.text.text = novel.Text;
-                await this.waitTapButton.Wait(token);
+                await this.button.OnClickAsObservable().TakeUntilDestroy(this);
                 //TODO 文字送り
                 //TODO ウィンドウ開閉アニメ
             }
