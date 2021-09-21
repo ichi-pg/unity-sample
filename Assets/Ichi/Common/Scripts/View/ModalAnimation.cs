dd0053413040f36009f23307d6e255e4fbc6f82a
ichi-pg <ichi.colors@gmail.com>
2021-09-21 18:05:36 +0900

    modal fade

--- Assets/Ichi/Common/Scripts/View/ModalAnimation.cs
+++ Assets/Ichi/Common/Scripts/View/ModalAnimation.cs
@@ -9,15 +9,19 @@ namespace Ichi.Common
 {
     public class ModalAnimation : MonoBehaviour
     {
+        [SerializeField]
+        private CanvasGroup group;
+
         void Start() {
             this.transform.localScale = Vector3.zero;
             this.transform.DOScale(1f, 0.1f);
+            this.group.DOFade(0f, 0.1f).From();
         }
 
         public void Close(Action onComplete) {
-            // this.transform.DOScale(0f, 0.1f).OnComplete(() => {
+            this.group.DOFade(0f, 0.1f).OnComplete(() => {
                 onComplete();
-            // });
+            });
         }
     }
 }
